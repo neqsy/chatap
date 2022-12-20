a1ae75e7b946aad89449aa3a6b0e49bec9ad044b
Paweł Witowski <neqsy98@gmail.com>
2022-12-20 14:50:44 +0100

    Added registration page. Added routes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Login from './pages/Login';
+import Registration from './pages/Registration';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="registration" element={<Registration />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Login = () => {
+const Registration = () => {
     return (
         <div class="container bg-transparent">
             <div class="row justify-content-center">
@@ -13,9 +13,13 @@ const Login = () => {
                         <div class="card-body py-4 px-5">
                             <h1 class="fs-4 card-title fw-bold mb-4 text-center">Chatap</h1>
                             <form>
+                            <div class="mb-3">
+                                    <label class="mb-2" for="displayname">Display Name</label>
+                                    <input id="displayname" type="text" class="form-control shadow-sm rounded-4" name="displayname" autofocus/>
+                                </div>
                                 <div class="mb-3">
                                     <label class="mb-2" for="email">Email</label>
-                                    <input id="name" type="text" class="form-control shadow-sm rounded-4" name="name" autofocus/>
+                                    <input id="email" type="text" class="form-control shadow-sm rounded-4" name="email" autofocus/>
                                 </div>
                                 <div class="mb-3">
                                     <div class="mb-2 w-100">
@@ -23,13 +27,17 @@ const Login = () => {
                                     </div>
                                     <input id="password" type="password" class="form-control shadow-sm rounded-4" name="password"/>
                                 </div>
-                                <button class="btn btn-blue w-100 rounded-4">Log in</button>
+                                <div class="mb-3">
+                                <button class="btn btn-white w-100 mt-4 py-2 rounded-4"><i class="fa-solid fa-user-plus"></i>
+                                        <label for="password">Add avatar</label></button>
+                                </div>
+                                <button class="btn btn-blue w-100 rounded-4">Register</button>
                             </form>
                         </div>
                     </div>
                     
                     <p class="text-center text-white mt-3">
-                        Don't have an account? <a href="/registration" class="link-info">Sign up</a>
+                        You do have an account? <a href="/" class="link-info">Log in</a>
                     </p>
                 </div>
             </div>
@@ -37,4 +45,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Registration;
