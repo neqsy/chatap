bb2ecb3124c75284cd1b464cab28e85562c0f056
PrzemekBarczyk <przemek.barczyk@gmail.com>
2022-12-30 17:45:04 +0100

    Registration with email and password

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
@@ -13,4 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth()
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,12 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
+import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
+    const [err, setErr] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const avatar = e.target[3].files[0];
+
+        try {
+            // create user
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+
+            // create unique image name
+            const storageRef = ref(storage, res.user.uid);
+
+            await uploadBytesResumable(storageRef, avatar).then(() => {
+                getDownloadURL(storageRef).then(async (downloadURL) => {
+                    try {
+                        // update profile
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL
+                        });
+    
+                        // create user on firestore
+                        await setDoc(doc(db, "users", res.user.uid), {
+                            uid: res.user.uid,
+                            displayName,
+                            email,
+                            photoURL: downloadURL
+                        });
+    
+                        // create enmpty user chats on firestore
+                        await setDoc(doc(db, "userChats", res.user.uid), {});
+    
+                        // go to home page
+                        navigate("/");
+                    } catch (err) {
+                        console.log(err);
+                        setErr(true);
+                        setLoading(false);
+                    }
+                });
+            });
+        }
+        catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+        }
+    }    
+
     return (
         <Container className="bg-transparent">
             <Row className="justify-content-center">
@@ -18,7 +79,7 @@ const Registration = () => {
                     <Card className="shadow-lg rounded-4">
                         <Card.Body className="py-4 px-5">
                             <Card.Title className="fs-4 fw-bold mb-4 text-center">Chatap</Card.Title>
-                            <Form>
+                            <Form onSubmit={handleSubmit}>
                                 <Form.Group className="mb-3">
                                     <Form.Label className="mb-2">Display name</Form.Label>
                                     <Form.Control id="displayname" type="text" className="shadow-sm rounded-4" name="displayname" autoFocus/>
@@ -37,13 +98,15 @@ const Registration = () => {
                                         <i className="fa-solid fa-user-plus"></i>Add avatar
                                     </Form.Label>
                                 </Form.Group>
-                                <Button className="btn-blue w-100 rounded-4">Register</Button>
+                                <Button className="btn-blue w-100 rounded-4" type="submit">Register</Button>
+                                {loading && <div className="w-100 mt-1 text-center text-primary">"Uploading and compressing the image. Please wait..."</div>}
+                                {err && <div className="w-100 mt-1 text-center text-danger">Something went wrong</div>}
                             </Form>
                         </Card.Body>
                     </Card>
                     
-                    <p class="text-center text-white mt-3">
-                        You do have an account? <a href="/" class="link-info">Log in</a>
+                    <p className="text-center text-white mt-3">
+                        You do have an account? <a href="/" className="link-info">Log in</a>
                     </p>
                 </Col>
             </Row>
@@ -52,4 +115,3 @@ const Registration = () => {
 };
 
 export default Registration;
-
