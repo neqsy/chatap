464088277b2c3824ca129672412bab73becf02bd
PrzemekBarczyk <przemek.barczyk@gmail.com>
2022-12-30 19:29:57 +0100

    Forbid access to home page for unauthorized users

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import Login from './pages/Login';
 import Registration from './pages/Registration';
 import Home from './pages/Home';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from './context/AuthContext';
+import { useContext } from 'react';
 
 function App() {
+  const {currentUser} = useContext(AuthContext);
+
+  const ProtectedRoute = ({children}) => {
+    if (!currentUser) {
+      return <Navigate to="/login"/>
+    }
+    return children;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route index element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="login" element={<Login />} />
           <Route path="registration" element={<Registration />} />
         </Route>
