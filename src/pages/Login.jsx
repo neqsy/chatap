d7bc2f9492de424dc3d3a64ed84b1dcd3a5ae898
PrzemekBarczyk <przemek.barczyk@gmail.com>
2022-12-30 14:05:02 +0100

    Fix login page
    - changed class to className
    - removed controlId

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,19 +12,19 @@ const Login = () => {
             <Row className="justify-content-center">
                 <Col className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
 
-                    <Button className="btn-white w-100 mt-4 py-2 rounded-4">Log in with Facebook <i class="fa-brands fa-square-facebook"></i></Button>
-                    <Button className="btn-white w-100 my-4 py-2 rounded-4">Log in with Google <i class="fa-brands fa-google"></i></Button>
+                    <Button className="btn-white w-100 mt-4 py-2 rounded-4">Log in with Facebook <i className="fa-brands fa-square-facebook"></i></Button>
+                    <Button className="btn-white w-100 my-4 py-2 rounded-4">Log in with Google <i className="fa-brands fa-google"></i></Button>
                     
                     <Card className="shadow-lg rounded-4">
                         <Card.Body className="py-4 px-5">
                             <Card.Title className="fs-4 fw-bold mb-4 text-center">Chatap</Card.Title>
                             <Form>
-                                <Form.Group className="mb-3" controlId="email">
+                                <Form.Group className="mb-3">
                                     <Form.Label className="mb-2">Email</Form.Label>
-                                    <Form.Control id="name" type="text" className="shadow-sm rounded-4" name="name" autofocus/>
+                                    <Form.Control id="name" type="text" className="shadow-sm rounded-4" name="name" autoFocus/>
                                 </Form.Group>
                                 <Form.Group className="mb-3">
-                                    <Form.Label class="mb-2">Password</Form.Label>
+                                    <Form.Label className="mb-2">Password</Form.Label>
                                     <Form.Control id="password" type="password" className="form-control shadow-sm rounded-4" name="password"/>
                                 </Form.Group>
                                 <Button className="btn-blue w-100 rounded-4">Log in</Button>
@@ -32,8 +32,8 @@ const Login = () => {
                         </Card.Body>
                     </Card>
                     
-                    <p class="text-center text-white mt-3">
-                        Don't have an account? <a href="/registration" class="link-info">Sign up</a>
+                    <p className="text-center text-white mt-3">
+                        Don't have an account? <a href="/registration" className="link-info">Sign up</a>
                     </p>
                 </Col>
             </Row>
