e7eccec89a7173de8d7a7fcdec7ed3392dc073c6
PrzemekBarczyk <przemek.barczyk@gmail.com>
2022-12-30 17:02:58 +0100

    Add empty home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Login from './pages/Login';
 import Registration from './pages/Registration';
+import Home from './pages/Home';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,8 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="registration" element={<Registration />}>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="registration" element={<Registration />} />
         </Route>
       </Routes>
     </BrowserRouter>
